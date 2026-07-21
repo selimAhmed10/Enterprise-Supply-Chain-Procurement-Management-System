ec8d139a440866f279ec72c61ac6c8c492dc7dd4
Md Selim Ahmed <selim174662@gmail.com>
2026-07-21 11:31:31 +0600

    feat: add the database connection all database info will take from the env file

--- backend/scm_pro/settings.py
+++ backend/scm_pro/settings.py
@@ -120,8 +120,12 @@ WSGI_APPLICATION = 'scm_pro.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME', 'scm_db'),
+        'USER': os.getenv('DB_USER', 'scm_user'),
+        'PASSWORD': os.getenv('DB_PASSWORD', 'scm_password'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
 
