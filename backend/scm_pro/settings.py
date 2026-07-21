3f4669c8ab3f826f2ea3fce54328c5fcce8eb167
Md Selim Ahmed <selim174662@gmail.com>
2026-07-21 11:33:48 +0600

    feat: in settings add the static file configuration

--- backend/scm_pro/settings.py
+++ backend/scm_pro/settings.py
@@ -165,6 +165,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
