249b007e4c679bfd28747219fa18e9b28265adbf
Md Selim Ahmed <selim174662@gmail.com>
2026-07-21 11:17:31 +0600

    feat: add the load env data,build path and security settings

--- backend/scm_pro/settings.py
+++ backend/scm_pro/settings.py
@@ -30,6 +30,17 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+# Load environment variables from .env file
+load_dotenv()
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# SECURITY SETTINGS
+
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-er3-9a#h_r*eu4ld*cdl5(96+k*mz@)hwm!dkk=@9vnpd@^pgn')
+DEBUG = os.getenv('DEBUG', 'True') == 'True'
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 # Application definition
 
